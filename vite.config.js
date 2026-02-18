0018f526a9f3aba60d40b639f89b93795c66624a
dikshant1995 <diksa21@gmail.com>
2026-02-18 15:50:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
   },
   // Only build from main index.html, exclude test files
   build: {
-    outDir: 'build',
     rollupOptions: {
       input: {
         main: './index.html'
