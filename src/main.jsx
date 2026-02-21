4992e9be4363e2adb20602b79da5de2dbfc914a0
dikshant1995 <diksa21@gmail.com>
2026-02-21 16:44:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import CustomerFacingApp from './CustomerFacingApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CustomerFacingApp />
+    <HelmetProvider>
+      <BrowserRouter>
+        <CustomerFacingApp />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
 )
