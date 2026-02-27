3ac0f2517de4913e8522b98260a79dad3035789f
dikshant1995 <diksa21@gmail.com>
2026-02-27 21:39:10 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9D1bKRLvS6GwnJLfjRDBjMkKkQtvlifE",
-    authDomain: "laxmi-credit-enterprise.firebaseapp.com",
-    projectId: "laxmi-credit-enterprise",
-    storageBucket: "laxmi-credit-enterprise.firebasestorage.app",
-    messagingSenderId: "70791817257",
-    appId: "1:70791817257:web:8c0bf346d9636905e41438",
-    measurementId: "G-J1R46P3Z81"
+    apiKey: "AIzaSyCRcHH37y5rLy34xSW1OdSy5MklnSnuO6o",
+    authDomain: "laxmi-credit.firebaseapp.com",
+    projectId: "laxmi-credit",
+    storageBucket: "laxmi-credit.firebasestorage.app",
+    messagingSenderId: "439589007843",
+    appId: "1:439589007843:web:63d32a89b258686144f0d6"
 };
 
 // Initialize Firebase
