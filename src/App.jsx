4deaca4d93213e0e1310f130819470f258371ed0
dikshant1995 <diksa21@gmail.com>
2026-02-28 14:06:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function App() {
   return (
     <div className="App">
       {showAdminDashboard ? (
-        <AdminDashboard onBackToCustomer={() => setShowAdminDashboard(false)} />
+        <AdminDashboard />
       ) : (
         <>
           <header>
