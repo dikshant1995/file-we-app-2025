6916d08cdd0c1e923a5da7ed00f70332b3f91af1
dikshant1995 <diksa21@gmail.com>
2026-02-28 15:47:35 +0530

    Update calculator.js

--- backend/calculators/axis-fin/calculator.js
+++ backend/calculators/axis-fin/calculator.js
@@ -1,4 +1,5 @@
 import { axisFinConfig } from './config.js';
+import { getBankConfig } from '../../utils/configHelper.js';
 
 // Function to calculate EMI
 const calculateEMI = (principal, annualInterestRate, tenureInYears) => {
