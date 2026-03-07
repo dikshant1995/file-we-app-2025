14e8c1616a67cccc5c21741ae497cc73cc44ab0b
dikshant1995 <diksa21@gmail.com>
2026-03-07 19:18:17 +0530

    Update BlogHome.jsx

--- src/components/BlogHome.jsx
+++ src/components/BlogHome.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { blogPosts } from '../data/blogData';
+import { blogPosts } from '../data/blogData.js';
 import './Blog.css';
 
 const BlogHome = () => {
